85dbb617f0bfa244e6380ea68df9899720f2c4a5
Simon <simon.helie-dabate@hotmail.com>
2018-10-26 20:56:52 -0400

    Agreabilite 1.0 + AgreabiliteController 0.1
    Testing push.

---
+++ CGICodeJamA2018CHSS/CGICodeJamA2018CHSS/Agreabilite.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGICodeJamA2018CHSS
+{
+    public class Agreabilite
+    {
+        private string name;
+        private int xpValue;
+        private int goldValue;
+
+        Agreabilite(string nom, int xp, int gold) {
+            setName(nom);
+            setXp(xp);
+            setGold(gold);
+        }
+
+        public string getName() {
+            return name;
+        }
+
+        private void setName(string nom) {
+            name = nom;
+        }
+
+        public int getXp()
+        {
+            return xpValue;
+        }
+
+        private void setXp(int xp)
+        {
+            xpValue = xp;
+        }
+
+        public int getGold()
+        {
+            return goldValue;
+        }
+
+        private void setGold(int gold)
+        {
+            goldValue = gold;
+        }
+    }
+}
---
+++ CGICodeJamA2018CHSS/CGICodeJamA2018CHSS/AgreabiliteController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGICodeJamA2018CHSS
+{
+    static public class AgreabiliteController
+    {
+        static private List<Agreabilite> listeActions = new List<Agreabilite>();
+        static private List<Utilisateur> userList = new List<Utilisateur>();
+        static private int maxGoldParDon = 0;
+
+        static public void pushAgreabilite(Agreabilite agreabilite) {
+            listeActions.Add(agreabilite);
+            listeActions.TrimExcess();
+        }
+
+        static public void popAgreabilite(Agreabilite agreabilite) {
+            if (listeActions.Contains(agreabilite)) {
+                listeActions.Remove(agreabilite);
+            }
+        }
+
+        static public List<Utilisateur> getListeUtilisateurs() {
+            return userList;
+        }
+
+        static private void updateListeUtilisateurs() {
+            //userList = //entrer nom du get ListeUtilisateurs
+        }
+
+        static public void giveReward(Utilisateur giver, Utilisateur receiver, int amount) {
+            if (giver != receiver && amount > 0 && maxGoldParDon >= amount) {
+                //receiver.getGold(amount);
+            }
+        }
+
+
+    }
+}
